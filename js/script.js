437fea37ba29edbf64c23dea3be270d933dcb22e
Mari Sumida Miyashiro <mariksm@hawaii.edu>
2024-04-07 16:52:21 -1000

    added more images, updated case studies

--- js/script.js
+++ js/script.js
@@ -10,3 +10,18 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
