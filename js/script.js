41ea664d152e622388ca44d6f21ef65808e9e40e
Mari Sumida Miyashiro <mariksm@hawaii.edu>
2024-04-18 16:50:20 -1000

    updated to new portfolio site

--- js/script.js
+++ js/script.js
@@ -1,27 +1,44 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const listItems = document.querySelectorAll(".site-nav li");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const listItems = document.querySelectorAll(".site-nav li");
 
-    listItems.forEach((listItem) => {
-        listItem.addEventListener("click", () => {
-            listItems.forEach((otherItem) => {
-                otherItem.classList.remove('active')
-            })
-            listItem.classList.add('active')
-        });
+//     listItems.forEach((listItem) => {
+//         listItem.addEventListener("click", () => {
+//             listItems.forEach((otherItem) => {
+//                 otherItem.classList.remove('active')
+//             })
+//             listItem.classList.add('active')
+//         });
+//     });
+// });
+
+if (document.querySelector(".swiper")) {
+    new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        autoplay: {
+            delay: 2000,
+        },
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+}
+
+
+// CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
+const myobserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.setAttribute("data-inview", "");
+        } else {
+            entry.target.removeAttribute("data-inview");
+        }
     });
 });
 
-
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    autoplay: {
-        delay: 2000,
-    },
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
+Array.from(document.querySelectorAll('.section-line'))?.forEach((el) => {
+    myobserver.observe(el);
 });
